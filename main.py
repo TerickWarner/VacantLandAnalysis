92948c6804fc06aa8b52058efb620903a3121f53
TerickWarner-AMEND <89525088+TerickWarner-AMEND@users.noreply.github.com>
2022-11-17 01:47:58 -0500

    Final Update

--- main.py
+++ main.py
@@ -57,11 +57,13 @@ def get_land_dotcom_for_sale_data():
     final_land_dotcom_df.rename(columns={0:'Acres', 1: 'Price'}, inplace=True)
     print(final_land_dotcom_df)
     final_land_dotcom_df['sale_sold'] = 'For Sale'
-    final_land_dotcom_df['Source'] = "Land.com"
+    final_land_dotcom_df['source'] = "Land.com"
     print(final_land_dotcom_df)
 
-    final_land_dotcom_df.to_csv("C:/Users/teric/Desktop/vacantLandData/LandDotComForSaleDataFINAL.csv")
+    # final_land_dotcom_df.to_csv("C:/Users/teric/Desktop/vacantLandData/LandDotComForSaleDataFINAL.csv")
+    
     driver.close()
+    return final_land_dotcom_df
 
 def get_land_dotcom_for_sold_data():
     #use the geckodriver to get to the specific url to find land.com for sale data
@@ -97,85 +99,278 @@ def get_land_dotcom_for_sold_data():
     final_land_dotcom_df.rename(columns={0:'Acres', 1: 'Price'}, inplace=True)
     print(final_land_dotcom_df)
     final_land_dotcom_df['sale_sold'] = 'sold'
-    final_land_dotcom_df['Source'] = "Land.com"
+    final_land_dotcom_df['source'] = "Land.com"
     print(final_land_dotcom_df)
 
-    final_land_dotcom_df.to_csv("C:/Users/teric/Desktop/vacantLandData/LandDotComForSoldDataFINAL.csv")
+    # final_land_dotcom_df.to_csv("C:/Users/teric/Desktop/vacantLandData/LandDotComForSoldDataFINAL.csv")
+    
     driver.close()
+    return final_land_dotcom_df
 
-# get_land_dotcom_for_sale_data()
-# get_land_dotcom_for_sold_data()
+land_sale = get_land_dotcom_for_sale_data()
+land_sold = get_land_dotcom_for_sold_data()
+
+def get_zillow_for_sale_data():
+    #use the geckodriver to get to the specific url to find land.com for sale data
+    options = Options()
+    service = Service("C:/Users/teric/Desktop/geckodriver.exe") #path to local geckodriver file which allows selenium to run
+    options.headless = False #runs it headless
+    driver = webdriver.Firefox(service=service, options=options)
+    driver.get("https://www.zillow.com/crawford-county-ga/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3A%22Crawford%20County%2C%20GA%22%2C%22mapBounds%22%3A%7B%22west%22%3A-84.35536400316056%2C%22east%22%3A-83.55198875901993%2C%22south%22%3A32.452387492077776%2C%22north%22%3A32.9297118218284%7D%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A2227%2C%22regionType%22%3A4%7D%5D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22sort%22%3A%7B%22value%22%3A%22days%22%7D%2C%22ah%22%3A%7B%22value%22%3Atrue%7D%2C%22sf%22%3A%7B%22value%22%3Afalse%7D%2C%22tow%22%3A%7B%22value%22%3Afalse%7D%2C%22mf%22%3A%7B%22value%22%3Afalse%7D%2C%22con%22%3A%7B%22value%22%3Afalse%7D%2C%22apco%22%3A%7B%22value%22%3Afalse%7D%2C%22apa%22%3A%7B%22value%22%3Afalse%7D%2C%22manu%22%3A%7B%22value%22%3Afalse%7D%2C%22lot%22%3A%7B%22min%22%3A87120%2C%22max%22%3A2178000%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A11%7D")
+    #let the data load into the browser with the sleep function
+    time.sleep(5)
+    #the data in located in a ul element within a bunch of li elements find the ul then find the li elements
+    zillow_ul = driver.find_element(By.XPATH, "/html/body/div[1]/div[5]/div/div/div[1]/div[1]/ul")
+    zillow_children = zillow_ul.find_elements(By.TAG_NAME, "li")
+    print(len(zillow_children))
+    empty_list = []
+    #create a list of lists with the appropriate data
+    for items in zillow_children:
+        string_items = items.get_attribute("innerText")
+        split_string_items = string_items.split('\n')
+        print(type(string_items))
+        empty_list.append(split_string_items)
+        
+    print(empty_list)
+
+    #transforming data, eleminating unneccssary data
+    substring = "Listing provided"
+    substring2 = 'Loading'
+    substring3 = 'Price cut'
+    for i in range(len(empty_list)):
+        x = empty_list[i]
+        for item in x.copy():
+            if substring in item:
+                x.remove(item)
+        for item in x.copy():
+            if substring2 in item:
+                x.remove(item)
+        for item in x.copy():
+            if substring3 in item:
+                x.remove(item)
+
+    #getting rid of empty lists within the list of lists
+    delete_empty_lists = [ele for ele in empty_list if ele != []]
+    delete_empty_lists2 = [ele for ele in delete_empty_lists if ele != ['']]
+    print(f"THIS IS THE FINAL LIST: {delete_empty_lists2}")
+    #creating the initial dataframe before transforming
+    initial_zillow_df = pd.DataFrame(delete_empty_lists2, columns=['Address', 'Price', 'Acres', 'days_on_zillow'])
+    print(initial_zillow_df)
+    #locating the acres column to split it by a seperator
+    acres_column_loc = initial_zillow_df.columns.get_loc('Acres')
+    seperator = 's'
+    split_initial_zillow_df = initial_zillow_df['Acres'].str.split(seperator, 1, expand=True)[0] + seperator
+    print(split_initial_zillow_df)
+    #creating final df
+    final_zillow_dotcom_df = pd.concat([initial_zillow_df.iloc[:, :acres_column_loc], split_initial_zillow_df, initial_zillow_df.iloc[:, acres_column_loc+1:]], axis=1)
+    print(final_zillow_dotcom_df)
+    final_zillow_dotcom_df.rename(columns={0:'Acres'}, inplace=True)
+    print(final_zillow_dotcom_df)
+    #adding data that shows if it was for sale or sold and source of info
+    final_zillow_dotcom_df['sale_sold'] = 'sale'
+    final_zillow_dotcom_df['source'] = "zillow.com"
+    print(final_zillow_dotcom_df)
+
+    # final_zillow_dotcom_df.to_csv("C:/Users/teric/Desktop/vacantLandData/ZillowDOTcomLandForSaleFINAL.csv")
+    driver.close()
+    return final_zillow_dotcom_df
 
 
-def get_land_watch_for_sale_data():
+def get_zillow_for_sold_data():
+    #use the geckodriver to get to the specific url to find land.com for sale data
+    options = Options()
+    service = Service("C:/Users/teric/Desktop/geckodriver.exe") #path to local geckodriver file which allows selenium to run
+    options.headless = False #runs it headless
+    driver = webdriver.Firefox(service=service, options=options)
+    driver.get("https://www.zillow.com/crawford-county-ga/sold/?searchQueryState=%7B%22usersSearchTerm%22%3A%22Crawford%20County%2C%20GA%22%2C%22mapBounds%22%3A%7B%22west%22%3A-85.5604268693715%2C%22east%22%3A-82.346925892809%2C%22south%22%3A31.74799152969877%2C%22north%22%3A33.62488170826132%7D%2C%22mapZoom%22%3A9%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A2227%2C%22regionType%22%3A4%7D%5D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22con%22%3A%7B%22value%22%3Afalse%7D%2C%22apa%22%3A%7B%22value%22%3Afalse%7D%2C%22mf%22%3A%7B%22value%22%3Afalse%7D%2C%22ah%22%3A%7B%22value%22%3Atrue%7D%2C%22sort%22%3A%7B%22value%22%3A%22days%22%7D%2C%22lot%22%3A%7B%22min%22%3A87120%2C%22max%22%3A2178000%7D%2C%22sf%22%3A%7B%22value%22%3Afalse%7D%2C%22tow%22%3A%7B%22value%22%3Afalse%7D%2C%22manu%22%3A%7B%22value%22%3Afalse%7D%2C%22apco%22%3A%7B%22value%22%3Afalse%7D%2C%22rs%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%7D")
+    #let the data load into the browser with the sleep function
+    time.sleep(5)
+    #the data in located in a ul element within a bunch of li elements find the ul then find the li elements
+    zillow_ul = driver.find_element(By.XPATH, "/html/body/div[1]/div[5]/div/div/div[1]/div[1]/ul")
+    zillow_children = zillow_ul.find_elements(By.TAG_NAME, "li")
+    print(len(zillow_children))
+    empty_list = []
+    #create a list of lists with the appropriate data
+    for items in zillow_children:
+        string_items = items.get_attribute("innerText")
+        split_string_items = string_items.split('\n')
+        print(type(string_items))
+        empty_list.append(split_string_items)
+        
+    print(empty_list)
+
+    #transforming data, eleminating unneccssary data
+    substring = "Listing provided"
+    substring2 = 'Loading'
+    substring3 = 'Price cut'
+    for i in range(len(empty_list)):
+        x = empty_list[i]
+        for item in x.copy():
+            if substring in item:
+                x.remove(item)
+        for item in x.copy():
+            if substring2 in item:
+                x.remove(item)
+        for item in x.copy():
+            if substring3 in item:
+                x.remove(item)
+
+    #getting rid of empty lists within the list of lists
+    delete_empty_lists = [ele for ele in empty_list if ele != []]
+    delete_empty_lists2 = [ele for ele in delete_empty_lists if ele != ['']]
+    print(f"THIS IS THE FINAL LIST: {delete_empty_lists2}")
+    #creating the initial dataframe before transforming
+    initial_zillow_df = pd.DataFrame(delete_empty_lists2, columns=['Address', 'Price', 'Acres', 'days_on_zillow'])
+    print(initial_zillow_df)
+    #locating the acres column to split it by a seperator
+    acres_column_loc = initial_zillow_df.columns.get_loc('Acres')
+    seperator = 's'
+    split_initial_zillow_df = initial_zillow_df['Acres'].str.split(seperator, 1, expand=True)[0] + seperator
+    print(split_initial_zillow_df)
+    #creating final df
+    final_zillow_dotcom_df = pd.concat([initial_zillow_df.iloc[:, :acres_column_loc], split_initial_zillow_df, initial_zillow_df.iloc[:, acres_column_loc+1:]], axis=1)
+    print(final_zillow_dotcom_df)
+    final_zillow_dotcom_df.rename(columns={0:'Acres'}, inplace=True)
+    print(final_zillow_dotcom_df)
+    #adding data that shows if it was for sale or sold and source of info
+    final_zillow_dotcom_df['sale_sold'] = 'sold'
+    final_zillow_dotcom_df['source'] = "zillow.com"
+    print(final_zillow_dotcom_df)
+
+    # final_zillow_dotcom_df.to_csv("C:/Users/teric/Desktop/vacantLandData/ZillowDOTcomLandForSoldFINAL.csv")
+    driver.close()
+    return final_zillow_dotcom_df
+
+
+zil_sale = get_zillow_for_sale_data()
+zil_sold = get_zillow_for_sold_data()
+
+def get_trulia_for_sale_data():
     # use the geckodriver to get to the specific url to find land.com for sale data
     options = Options()
     service = Service("C:/Users/teric/Desktop/geckodriver.exe") #path to local geckodriver file which allows selenium to run
     options.headless = False #runs it headless
     driver = webdriver.Firefox(service=service, options=options)
-    driver.get("https://www.trulia.com/for_sale/13079_c/LOT%7CLAND_type/")
+    driver.get("https://www.trulia.com/for_sale/13079_c/LOT%7CLAND_type/2p_ls/")
+    time.sleep(5)
+    #first we find the ul with the list of data that we need
+    trulia_ul = driver.find_element(By.XPATH, "/html/body/div[2]/div[2]/div/div[2]/div[1]/div[1]/ul")
+    #the length of the trulia_li will tell us how much data we have without getting into the 'Similar vacant lots near you" data
+    trulia_li = trulia_ul.find_elements(By.TAG_NAME, 'li')
+    time.sleep(3)
+    #Get a list of the acres data
+    acres_list = []
+    for i in range(len(trulia_li)):
+        try:
+            acres = trulia_ul.find_element(By.XPATH, f"/html/body/div[2]/div[2]/div/div[2]/div[1]/div[1]/ul/li[{i+1}]/div/div/div/div/div[1]/div[3]/span[2]/span")
+            acres_list.append(acres.text)
+        except:
+            try:
+                acres = trulia_ul.find_element(By.CSS_SELECTOR, f"div.SearchResultsList__Container-sc-14hv67h-0:nth-child(1) > ul:nth-child(2) > li:nth-child({i+1}) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > span:nth-child(1) > span:nth-child(1)")
+                acres_list.append(acres.text)
+            except:
+                pass
+    print(acres_list)
+    #get a list of the price data
+    time.sleep(5)
+    price_list = []
+    for i in range(len(trulia_li)):
+        try:
+            price = trulia_ul.find_element(By.CSS_SELECTOR, f"div.SearchResultsList__Container-sc-14hv67h-0:nth-child(1) > ul:nth-child(2) > li:nth-child({i+1}) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)")
+            price_list.append(price.text)
+        except:
+            pass
+    print(price_list)
+    #get a list of the address data
+    time.sleep(3)
+    address_list = []
+    for i in range(len(trulia_li)):
+        try:
+            address = trulia_ul.find_element(By.CSS_SELECTOR, f"div.SearchResultsList__Container-sc-14hv67h-0:nth-child(1) > ul:nth-child(2) > li:nth-child({i+1}) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > a:nth-child(3) > div:nth-child(1)")
+            address_list.append(address.text)
+        except:
+            pass
+    print(address_list)
+    trulia_df = pd.DataFrame()
+    trulia_df['Acres'] = acres_list
+    trulia_df['Price'] = price_list
+    trulia_df['Address'] = address_list
+    trulia_df["Address"] = trulia_df["Address"].str.replace("\n", " ")
+    trulia_df['sale_sold'] = 'sale'
+    trulia_df['source'] = 'trulia.com'
 
-get_land_watch_for_sale_data()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def get_zillow_for_sale_data():
-#     #use the geckodriver to get to the specific url to find land.com for sale data
-#     options = Options()
-#     service = Service("C:/Users/teric/Desktop/geckodriver.exe") #path to local geckodriver file which allows selenium to run
-#     options.headless = False #runs it headless
-#     driver = webdriver.Firefox(service=service, options=options)
-#     driver.get("https://www.zillow.com/crawford-county-ga/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3A%22Crawford%20County%2C%20GA%22%2C%22mapBounds%22%3A%7B%22west%22%3A-84.35536400316056%2C%22east%22%3A-83.55198875901993%2C%22south%22%3A32.452387492077776%2C%22north%22%3A32.9297118218284%7D%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A2227%2C%22regionType%22%3A4%7D%5D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22sort%22%3A%7B%22value%22%3A%22days%22%7D%2C%22ah%22%3A%7B%22value%22%3Atrue%7D%2C%22sf%22%3A%7B%22value%22%3Afalse%7D%2C%22tow%22%3A%7B%22value%22%3Afalse%7D%2C%22mf%22%3A%7B%22value%22%3Afalse%7D%2C%22con%22%3A%7B%22value%22%3Afalse%7D%2C%22apco%22%3A%7B%22value%22%3Afalse%7D%2C%22apa%22%3A%7B%22value%22%3Afalse%7D%2C%22manu%22%3A%7B%22value%22%3Afalse%7D%2C%22lot%22%3A%7B%22min%22%3A87120%2C%22max%22%3A2178000%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A11%7D")
-#     zillow_body = driver.find_elements(By.XPATH, "//div[contains(@class, 'property-card-data')]")
-    
-#     time.sleep(5)
-#     print(zillow_body)
-#     print(f"THIS IS THE LENGTH OF THE ZILLOW BODY {len(zillow_body)}")
-#     counter = 1
-#     for i in zillow_body:
-#         print(i.text)
-#         counter += 1
-
-#     print(f"FINAL COUNTER {counter}")
-    # final_list = []
-    # for i in zillow_body:
-    #     acre = i.find_element(By.XPATH, "//*[contains(text(), 'acre')]").text
-    #     address = i.find_element(By.XPATH, "//address[contains(@data-test, 'property-card-addr')]").text
-    #     price = i.find_element(By.XPATH, "//span[contains(@data-test, 'property-card-price')]").text
-    #     child_list = [acre, address, price]
-    #     final_list.append(child_list)
-    
-    # zillow_for_sale_df = pd.DataFrame(final_list, columns=['Acres', 'Address', 'Price'])
-    # print(zillow_for_sale_df)
+    # trulia_df.to_csv("C:/Users/teric/Desktop/vacantLandData/TruliaForSaleFinal.csv")
+    driver.close()
+    return trulia_df
 
 
-# get_zillow_for_sale_data()
+def get_trulia_for_sold_data():
+    # use the geckodriver to get to the specific url to find land.com for sale data
+    options = Options()
+    service = Service("C:/Users/teric/Desktop/geckodriver.exe") #path to local geckodriver file which allows selenium to run
+    options.headless = False #runs it headless
+    driver = webdriver.Firefox(service=service, options=options)
+    driver.get("https://www.trulia.com/sold/13079_c/LOT%7CLAND_type/2p_ls/6_srl/")
+    time.sleep(5)
+    #first we find the ul with the list of data that we need
+    trulia_ul = driver.find_element(By.XPATH, "/html/body/div[2]/div[2]/div/div[2]/div[1]/div[1]/ul")
+    #the length of the trulia_li will tell us how much data we have without getting into the 'Similar vacant lots near you" data
+    trulia_li = trulia_ul.find_elements(By.TAG_NAME, 'li')
+    time.sleep(5)
+    #Get a list of the acres data
+    acres_list = []
+    for i in range(len(trulia_li)):
+        try:
+            acres = trulia_ul.find_element(By.XPATH, f"/html/body/div[2]/div[2]/div/div[2]/div[1]/div[1]/ul/li[{i+1}]/div/div/div/div/div[1]/div[3]/span[2]/span")
+            acres_list.append(acres.text)
+        except:
+            try:
+                acres = trulia_ul.find_element(By.CSS_SELECTOR, f"div.SearchResultsList__Container-sc-14hv67h-0:nth-child(1) > ul:nth-child(2) > li:nth-child({i+1}) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > span:nth-child(1) > span:nth-child(1)")
+                acres_list.append(acres.text)
+            except:
+                pass
+    print(acres_list)
+    #get a list of the price data
+    time.sleep(3)
+    price_list = []
+    for i in range(len(trulia_li)):
+        try:
+            price = trulia_ul.find_element(By.CSS_SELECTOR, f"div.SearchResultsList__Container-sc-14hv67h-0:nth-child(1) > ul:nth-child(2) > li:nth-child({i+1}) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)")
+            price_list.append(price.text)
+        except:
+            pass
+    print(price_list)
+    #get a list of the address data
+    time.sleep(3)
+    address_list = []
+    for i in range(len(trulia_li)):
+        try:
+            address = trulia_ul.find_element(By.CSS_SELECTOR, f"div.SearchResultsList__Container-sc-14hv67h-0:nth-child(1) > ul:nth-child(2) > li:nth-child({i+1}) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > a:nth-child(3) > div:nth-child(1)")
+            address_list.append(address.text)
+        except:
+            pass
+    print(address_list)
+    trulia_df = pd.DataFrame()
+    trulia_df['Acres'] = acres_list
+    trulia_df['Acres'] = 'None Found'
+    trulia_df['Price'] = price_list
+    trulia_df['Address'] = address_list
+    trulia_df["Address"] = trulia_df["Address"].str.replace("\n", " ")
+    trulia_df['sale_sold'] = 'sale'
+    trulia_df['source'] = 'trulia.com'
+    print(trulia_df)
 
+    # trulia_df.to_csv("C:/Users/teric/Desktop/vacantLandData/TruliaForSoldFinal.csv")
+    driver.close()
+    return trulia_df
+
+tru_sale = get_trulia_for_sale_data()
+tru_sold = get_trulia_for_sold_data()
+
+def concat_data(frames = [tru_sale, tru_sold, zil_sold, zil_sale, land_sale, land_sold]):
+    all_data_df = pd.concat(frames)
+    all_data_df.to_csv("C:/Users/teric/Desktop/vacantLandData/ALLDataFinal.csv")
+
+concat_data()
 
 
